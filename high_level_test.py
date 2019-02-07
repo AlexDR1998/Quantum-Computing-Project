da14c9a513f419699c9b4ef65f16999ec43dd5c0
Alex <alexander.richardson@gmail.com>
2019-02-07 14:02:48 +0000

    Added 3 qubit qft in high_level_test

--- high_level_test.py
+++ high_level_test.py
@@ -5,24 +5,28 @@ import matplotlib.pyplot as plt
 
 def main():
 	h = Hadamard()
-	b = CPhase(np.pi/4)
+	c2 = CPhase(np.pi/2)
+	c1 = CPhase(np.pi)
 	q = Qubit([1,0])
 	i = Identity()
 	v = V()
-
+	s = Swap()
 	#q1 = h*(q&q&q&q)
 
 	#q1.measure()
 	#print(q1)
-	alg = (h&h&h)*(b&i)*(v&i&v)*(i&h&i)
+	
+	qft = (i&i&h)*(i&s)*(c2&i)*(i&s)*(c1&i)*(i&h&i)*(c1&i)*(h&i&i)
 	
 
 	q_in = q&q&q
+	#q_in.measure()
 
 	hist = np.zeros(8)
 	n = 1000
 	for x in range(n):
-		q_out = alg*q_in
+		q_out = qft*q_in
+	#print(q_out)
 		hist = hist + q_out.measure() 
 		#print(q_out.measure())
 
