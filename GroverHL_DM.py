327447a7f0970efa5df50dadc63efed942b91875
dmathews98 <declanmathews@hotmail.com>
2019-02-08 13:49:10 +0000

    Some extra fixes since some gates were generalised to n

--- GroverHL_DM.py
+++ GroverHL_DM.py
@@ -21,8 +21,12 @@ def main():
     I = Identity()
     x = PauliX()
 
+    # --- HARDCODED VALUES; Adjust to I/O later ---
+    n = 3
+    target = 4
+
     # --- QReg size ---
-    n = 3   #HARDCODE; amend later
+    N = 2**n
     assert type(n) == int, "n must be an integer"
     assert n >= 2, "n must be greater than or equal to 2"
 
@@ -33,37 +37,33 @@ def main():
             q = q0&q
 
     # --- Target ---
-    desire = 6   #HARDCODE; amend later
-    target = desire - 1  #Fock space value
+    TAR = target - 1  #Fock space value is 1 above indice value
 
     # --- Iterations needed ---
-    its = 1   #NOTE: This is still a bit buggy when altering number of iterations;
-              #      need to go over and check, especially when Grover's iteration
-              #      is being sorted.
+    its = int((m.pi/4.0)*(N)**(1/2))
+    #NOTE: This is still a bit buggy; the target isn't achieved everytime.
+    #      Need to go over and check, especially when Grover's iteration is being sorted.
 
     # --- Grover gates --- #
     H = Hadamard(n)   #Hadamard all gates
     h1 = I&h   #Hadamard to one gate for n=2
-    X = x&x   #PauliX to two gates for n=2
+    X = PauliX(n)   #PauliX to n qubits
     if n > 2:
         for i in range(n-2):
             h1 = I&h1   #Hadamard to one gate for n>2
-            X = x&X   #PauliX to all gates for n>2
     cNOT = CNot()   #for 2 qubit Grover Iteration
     Tof = Toffoli()   #for 3 qubit Grover Iteration
-    O = Oracle(n, target)   #Oracle for qubit reg size 2^n and target value in Fock space
+    O = Oracle(n, TAR)   #Oracle for qubit reg size 2^n and target value in Fock space
 
     #NOTE: Section to make sure the Qreg gets put to |0>^&n
 
     # --- Formation of superposition from |0>&n state ---
     q = H*q
 
-    # --- Oracle Application ---
-    q = O*q
-
     # --- Grover's Iteration ---
-    #NOTE: This section I need to check what parts actually get looped;
-    #      I have a funny feeling that its only the bits inside the X's
+    #NOTE: This section I need to check what parts actually get looped
+
+    q = O*q   #Oracle application
     if n == 2:
         GI = H * X * h1 * cNOT * h1 * X * H
     elif n == 3:
