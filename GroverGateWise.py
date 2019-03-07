01780118a47f1871bb332eb7e5a250212cb78ea8
gralster <alstergrace@gmail.com>
2019-03-07 16:07:27 +0000

    fixed lazy and utils

--- GroverGateWise.py
+++ GroverGateWise.py
@@ -1,13 +1,10 @@
 import numpy as np
 import math as m
 import time as t
-import InOut as IO
-#from gatec import *
-from sparse import *
-
-def numits(n):
-    its = int((m.pi/4.0)*(2**n)**(1/2))
-    return its
+#import InOut as IO
+#import gatec as d
+#import sparse as s
+from lazy import *
 
 def findBinary(n, target):
     print('\nConverting Fock value to binary array...')
@@ -44,6 +41,8 @@ def grover(q, Search, cZ, H, X, its):
     #Create Superposition of states
     print('\nCreating superposition state...')
     t1 = t.time()
+    print(H)
+    print(q)
     q = H*q
     print('Creating superposition state took ' + str(t.time()-t1) + ' s')
 
@@ -51,6 +50,8 @@ def grover(q, Search, cZ, H, X, its):
     print('\nBeginning Grovers Iteration...')
     ti = t.time()
     for i in range(its):
+        print("Search is " + str(Search))
+        print("of type: " + str(type(Search)))
         q = Search*q
         q = cZ*q
         q = Search*q
@@ -67,10 +68,15 @@ def grover(q, Search, cZ, H, X, its):
     return q
 
 def main():
-    # --- How to run ---
-    io = IO.start()
-    n = io[0]
-    target = io[1]
+    n = int(input('How many qubits? '))
+    assert type(n) == int, "n must be an integer greater than or equal to 2"
+    assert n >= 2, "n must be an integer greater than or equal to 2"
+    N = 2**n
+    target = int(input('What Fock space value would you like to find? '))
+    assert type(target) == int, "Target must be an integer greater than or equal to 2"
+    assert target >= 0, "Target must be an integer greater than or equal to 0"
+    assert target <= N-1, "Target must be an integer less than or equal to " + str(N-1)
+
 
     # --- Timer Initialise ---
     t1 = t.time()
@@ -90,22 +96,24 @@ def main():
     t2 = t.time()
     q = Qubit(n)
     print('Quantum register formation took ' + str(t.time()-t2) + ' s')
-
+    print(q)
     # --- Number of Iterations calculation ---
-    its = numits(n)
+    its = int((m.pi/4.0)*(2**n)**(1/2))
 
     # --- Fock to Binary Array Conversion ---
     Binaryform = findBinary(n, target)
-
-    # --- Determining PauliX application to Qubits for Oracle ---
+    print(q)
+    # --- Oracle PauliX application dependent on Fock Target ---
     Search = oracleX(n, Binaryform, x, I)
-
+    print(q)
     # --- Create Superposition and Grover's Iteration ---
     q = grover(q, Search, cZ, H, X, its)
 
     # --- Measure and Display ---
     q.measure()
-    IO.printOut(q, target)
+    print('\nThe state of the ouput(in binary) is |' + str(q.split_register()) + '>')
+    print('The target state(in binary) was |' + str(bin(target)[2:]) + '>')
+    print('In Fock space this is |' + str(target) + '>')
     print('\nThis took '+str(t.time()-t1)+' s to run\n')
 
 main()
