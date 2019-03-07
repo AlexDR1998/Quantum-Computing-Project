9c56209f78642fc6015af9301c5351e2ab600f67
Alex <alexander.richardson@gmail.com>
2019-03-07 16:08:57 +0000

    More shors

--- shor.py
+++ shor.py
@@ -70,7 +70,7 @@ def QFT(N):
 	def _QFT(n):
 		#Method to return gate for n qubit fourier transform
 		#Probably best to do recursively
-		print(n)
+		#print(n)
 		if n==2:
 			#return ((Identity()&Hadamard())*(R(2))*(Hadamard()&Identity()))
 			return ((H()&I())*(R(2))*(I()&H()))#*Swap(2,0,1))
@@ -78,7 +78,7 @@ def QFT(N):
 		else:
 			g1 = _QFT(n-1)&I()
 			g2 = I(n)
-			print(range(n-2))
+			#print(range(n-2))
 			
 			for i in range(n-2):
 				#print(n-i)
@@ -86,7 +86,7 @@ def QFT(N):
 			#IO.Display(g2)
 			g3 = (I(n-2)&R(2))*(I(n-1)&H())
 			return (g1*g2*g3)
-	return _QFT(N)#*Flip(N)
+	return _QFT(N)*Flip(N)
 
 
 def iQFT(n):
@@ -104,7 +104,7 @@ def iQFT(n):
 
 			g3 = (I(n-1)&H())*(I(n-2)&R(2))
 			return (g3*g2*g1)
-	return _iQFT(n)
+	return Flip(n)*_iQFT(n)
 
 
 def GCD(x,y):
@@ -127,7 +127,15 @@ def extendedGCD(x,y):
 
 #def isPrime(n):
 #Method to check if a number is prime. Used to check input to shor's is not prime
+def modexp(b,e,N,qubits):
 
+	def _modexp(b,e,N):
+		t = 1
+		for i in range(e):
+			t = b*t
+		return t%N
+
+	return 
 
 def continued_fraction(y, Q, N):
 	#Not 100% sure what this does
@@ -185,18 +193,18 @@ def main():
 	#print(continued_fraction(1,93,416))
 	q1 = Qubit(8)
 	#print(q1.ret_mod())
-	q = (Hadamard(2)&Identity()&Hadamard(2)&Identity(3))*q1
+	q = (Hadamard(3)&Identity(5))*q1
 	
-	IO.Hist(q)
+	
 	
 	#print(q1)
 	#print(q2)
 	ft = QFT(8)
-	ift = iQFT(8)
+	#ift = iQFT(8)
 	#ift = iQFT(4)
 	#ft2 = HardQFT3()
 	#ift = iQFT(3)
-	IO.Display(ft*ft*ft*ft)
+	#IO.Display(ft)
 	#IO.Display(ft2)
 	#IO.Display(ift)
 	#IO.Display(ft*ft*ift*ift)
@@ -204,10 +212,27 @@ def main():
 	#print(ft.ret()-ft2.ret())
 	#plt.matshow((ft.ret()-ift.ret()).real)
 	#plt.show()
+	#29  17  493
+
+	f = np.vectorize(lambda x:modexp(23,x,493))
+	
+	xs = np.arange(0,256,1)
+	qreg = f(xs)/np.sum(xs).astype(float)
+	q = Qubit(qreg)
+	#IO.Hist(qres)
+	for x in range(100):
+		qres = ft*q
+		qres.measure()
+		r = int(str(qres.split_register()),2)
+		if r!=0 and GCD(r,493)!=1:
+			print(r)
+			
+	#plt.plot(xs,f(xs))
+	#plt.show()
 
 	#plt.matshow((ft.ret()-ift.ret()).imag)
 	#plt.show()
-	IO.Hist(ft*q)
+	#IO.LogHist(ft*q)
 	#IO.Display(Swap(4,0,3)*Swap(4,1,2))#*Swap(5,1,3))
 	#IO.Display(Flip(8))
 	#IO.Graph(ft*q)
